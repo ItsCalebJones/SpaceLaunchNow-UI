b9f1133323d2efa9d2897e34a9ccb3db4b654fe7
Caleb Jones <ca.jones9119@gmail.com>
2022-05-14 21:44:19 -0400

    tests: fix broken test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders Space Launch Now text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Get Launches/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement = screen.getAllByText(/Space Launch Now/);
+  expect(linkElement.lengeth) > 0;
 });
