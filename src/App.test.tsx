53f0f42322fe8072d93fcf3c2ba0bd2bde847f3b
Caleb Jones <cjones@tripwire.com>
2022-05-21 21:14:33 -0400

    tests: remove tests for now

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('Renders Space Launch Now text', () => {
-  render(<App />);
-  const linkElement = screen.getAllByText(/Space Launch Now/);
-  expect(linkElement).not.toBeNull();
-});
