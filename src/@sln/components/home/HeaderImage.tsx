5c4d6de24934736e2573f2dc8c6fc28453def269
Caleb Jones <ca.jones9119@gmail.com>
2022-06-01 22:51:08 -0400

    feat: wip current state

--- src/@sln/components/home/HeaderImage.tsx
+++ src/@sln/components/home/HeaderImage.tsx
@@ -2,12 +2,9 @@
 import React, { ReactElement, FC, useState } from "react";
 import {
   Box,
-  Button,
-  Container,
   Grid,
   Link,
   Theme,
-  Typography,
 } from "@mui/material";
 import { makeStyles } from "@sln/styles/";
 import { SLNTypography } from "@sln/components/SLNTypography";
@@ -25,7 +22,7 @@ import launcher from "@sln/assets/images/launcher_full.png";
 import appleMarketing from "@sln/assets/images/button/apple.png";
 import googleMarketing from "@sln/assets/images/button/google.png";
 
-import HR from "./HorizontalRule";
+import HR from "../utils/HorizontalRule";
 
 const urlBgImage = `url(${bgImage})`;
 
@@ -34,7 +31,7 @@ const urlBgImage = `url(${bgImage})`;
 }
 const useStyles = makeStyles()((theme: Theme) => ({
   header: {
-    backgroundImage: `-webkit-linear-gradient(135deg,rgba(71,85,97,0.55) 0,rgba(82,116,170,0.31) 100%),${urlBgImage};
+    backgroundImage: `-webkit-linear-gradient(135deg,rgba(71,85,97,0.55) 0,rgba(82,116,170,0.31) 100%), ${urlBgImage};
     background-image: linear-gradient(135deg,rgba(71,85,97,0.55) 0,rgba(82,116,170,0.31) 100%), ${urlBgImage}`,
     backgroundSize: "cover",
     backgroundPosition: "top",
@@ -43,6 +40,15 @@ const useStyles = makeStyles()((theme: Theme) => ({
     minHeight: "75vh",
     width: "100%",
   },
+  image: {
+    marginTop: "100px",
+    borderRadius: "50%",
+    boxShadow:
+      "0 16px 38px -12px rgb(0 0 0 / 56%), 0 4px 25px 0 rgb(0 0 0 / 12%), 0 8px 10px -5px rgb(0 0 0 / 20%)",
+  },
+  textShadow:{
+    filter: "drop-shadow(#00000059 2px 2px 4px)",
+  }
 }));
 
 const HeaderIcon: FC<any> = (): ReactElement => {
@@ -62,9 +68,7 @@ const HeaderIcon: FC<any> = (): ReactElement => {
   };
 
   return (
-    <Box 
-    data-test="header-image-bg"
-    className={classes.header}>
+    <Box data-test="header-image-bg" className={classes.header}>
       <Grid
         container
         xs={10}
@@ -80,12 +84,7 @@ const HeaderIcon: FC<any> = (): ReactElement => {
             data-test="header-image-logo"
             height={250}
             width={250}
-            style={{
-              marginTop: "50px",
-              borderRadius: "50%",
-              boxShadow:
-                "0 16px 38px -12px rgb(0 0 0 / 56%), 0 4px 25px 0 rgb(0 0 0 / 12%), 0 8px 10px -5px rgb(0 0 0 / 20%)",
-            }}
+            className={classes.image}
           />
         </Grid>
         <Grid item>
@@ -97,21 +96,23 @@ const HeaderIcon: FC<any> = (): ReactElement => {
             direction={"column"}
             justifyContent="center"
             alignItems="center"
+            textAlign="center"
           >
             <Grid item>
               <SLNTypography
                 data-test="header-image-title"
-                align="center"
-                kind="boldTitle"
+                kind="boldTitleWhite"
+                className={classes.textShadow}
               >
                 Space Launch Now
               </SLNTypography>
             </Grid>
             <Grid item>
-              <SLNTypography 
-              data-test="header-image-subtitle"
-              align="center" 
-              kind="subTitle">
+              <SLNTypography
+                data-test="header-image-subtitle"
+                kind="subTitleWhite"
+                className={classes.textShadow}
+              >
                 Spaceflight News and Events
               </SLNTypography>
             </Grid>
@@ -136,7 +137,7 @@ const HeaderIcon: FC<any> = (): ReactElement => {
             </Grid>
           </Grid>
 
-          <Grid item>
+          <Grid item mb={10}>
             <HR />
           </Grid>
         </Grid>
