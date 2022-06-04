ff79fb1fd05e34ece21754b63dc99f78741420d7
mmcdaniel44 <mmcdaniel44@gatech.edu>
2022-06-03 22:32:43 -0400

    Page header

--- src/@sln/pages/Events.tsx
+++ src/@sln/pages/Events.tsx
@@ -1,20 +1,27 @@
 import React, {ReactElement, FC} from "react";
-import {Box, Typography} from "@mui/material";
+import {Box, Container, Typography} from "@mui/material";
 import Base from "@sln/components/BaseLayout";
 import { Card } from "@mui/material";
-
+import { Divider } from "@mui/material";
+import { SLNTypography } from "@sln/components/SLNTypography";
+import { Grid } from "@mui/material";
 const Events: FC<any> = (): ReactElement => {
     return (
         <Base>
-            <Box sx={{
-                flexGrow: 1,
-                backgroundColor: 'whitesmoke',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center'
-            }}>
-            <Typography variant="h3">Events</Typography>
-            </Box>
+           <Container>
+           <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                style={{ minHeight: '100vh' }}
+                mt={25}
+            >
+                <Grid item xs={3}>
+                <Typography variant="h3">Upcoming Events</Typography>
+                </Grid>   
+            </Grid> 
+           </Container>
         </Base>
     );
 };
