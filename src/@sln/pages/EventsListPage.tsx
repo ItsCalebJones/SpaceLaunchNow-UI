9e200803fadefc5c674117e881350f96e0864188
mmcdaniel44 <mmcdaniel44@gatech.edu>
2022-06-14 20:26:53 -0400

    cleaning up code and page formatting

--- src/@sln/pages/EventsListPage.tsx
+++ src/@sln/pages/EventsListPage.tsx
@@ -25,9 +25,6 @@ import { width } from "@mui/system";
 const EventsListPage: FC<any> = (): ReactElement => {
 
     const {data: events, isLoading} = useEventList({limit: 5});
-
-
-
     return (
         <Base>
            <Container>
@@ -53,14 +50,11 @@ const EventsListPage: FC<any> = (): ReactElement => {
                     </Divider>   
                 </Box>
             </Stack> 
-
-
-
             {events?.data.results.map((event: Events) => (
                 <Stack direction="row" spacing={5} mt={5}>
                     <Card raised sx={{height:"250px",
                                         width:"368.88px"}}
-                    >
+                        >
                         <CardMedia
                         component="img"
                         sx={{height:'100%',
@@ -69,85 +63,36 @@ const EventsListPage: FC<any> = (): ReactElement => {
                         >
                         </CardMedia>
                     </Card>
-                    <Box>
-                        <CardContent>
-                            <Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
-                                <Grid item xs="auto">
-                                    <SLNTypography kind='eventCategory'>
-                                        {event.type.name}
-                                    </SLNTypography>
-                                </Grid>
-                                <Grid item xs="auto">
-                                    <SLNTypography kind='eventDate'>
-                                        {event.date}
-                                    </SLNTypography>
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <SLNTypography kind='eventTitle'>
-                                        {event.name}
-                                        </SLNTypography>
-                                </Grid>  
-                                <Grid item xs={12}>
-                                    <Chip label={event.location}/>
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <Typography variant="body2">
-                                    Text about event goes here
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <Button>Explore</Button>
-                                </Grid>
-                            </Grid>
-                        </CardContent>   
-                    </Box>
-                </Stack>
-                  
+                    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
+                        <Grid item xs="auto">
+                            <SLNTypography kind='eventCategory'>
+                                {event.type.name}
+                            </SLNTypography>
+                        </Grid>
+                        <Grid item xs="auto">
+                            <SLNTypography kind='eventDate'>
+                                {event.date}
+                            </SLNTypography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <SLNTypography kind='eventTitle'>
+                                {event.name}
+                            </SLNTypography>
+                        </Grid>  
+                        <Grid item xs={12}>
+                            <Chip label={event.location}/>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography variant="body2">
+                                {event.description}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Button>Explore</Button>
+                        </Grid>
+                    </Grid>
+                </Stack>           
                   ))}
-
-            {/* <Stack direction="row" spacing={2}>
-                    <Card raised>
-                        <CardMedia
-                        component="img"
-                        sx={{height: 250}}
-                        src=
-                        "https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/event_images/2021_nasa_astro_image_20211202132624.jpeg"
-                        >
-                        </CardMedia>
-                    </Card>
-                    <Box>
-                        <CardContent>
-                            <Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
-                                <Grid item xs="auto">
-                                    <SLNTypography kind='eventCategory'>
-                                        Press Event
-                                    </SLNTypography>
-                                </Grid>
-                                <Grid item xs="auto">
-                                    <SLNTypography kind='eventDate'>
-                                        JUN 12, 2022, NOON
-                                    </SLNTypography>
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <SLNTypography kind='eventTitle'>
-                                    2021 NASA Astronaut Candidates Announcement
-                                        </SLNTypography>
-                                    </Grid>
-                                <Grid item xs={12}>
-                                    <Chip label= "Ellington Field Joint Reserve Base"/>
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <Typography variant="body2">
-                                    Text about event goes here
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <Button>Explore</Button>
-                                </Grid>
-                            </Grid>
-                        </CardContent>
-                    </Box>
-            </Stack> */}
            </Container>
         </Base>
     );
