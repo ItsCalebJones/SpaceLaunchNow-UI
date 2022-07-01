48be21686a63ec9c1db1d8233b402fc1230d5702
mmcdaniel44 <mmcdaniel44@gatech.edu>
2022-06-30 22:07:26 -0400

    adding FunctionUtils with date_builder function (also to be used for adding other reusable functions)

--- src/@sln/pages/EventsListPage.tsx
+++ src/@sln/pages/EventsListPage.tsx
@@ -21,31 +21,13 @@ import { width } from "@mui/system";
 import { SLNButton } from "@sln/components/SLNButton";
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import SkeletonElement from "@sln/skeletons/SkeletonElement";
+import { date_builder } from "@sln/components/utils/FunctionUtils";
 
 
 
 
 const EventsListPage: FC<any> = (): ReactElement => {
-
-    const date_builder = function (s:string) : string{
-
-        // getting all of the date things :)
-        const date = new Date(s)
-        const month = date.toLocaleString('default', { month: 'long' });
-        const day = date.getDate()
-        const year = date.getFullYear()
-        const str_time = date.toLocaleString('en-US', {
-            hour: 'numeric',
-            minute: 'numeric',
-            hour12: true
-        })
-
-        // building date string from date things and returning date string
-        const date_string = `${month} ${day}, ${year}, ${str_time}`
-        return date_string
-    }
-
-    const {data: events, isLoading} = useEventList({limit: 100});
+    const {data: events, isLoading} = useEventList({limit: 10});
     return (
         <Base>
            <Container>
