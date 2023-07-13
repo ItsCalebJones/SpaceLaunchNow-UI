e633bb409e5e5bd5dc6c417b61553fa62c274786
Caleb Jones <ca.jones9119@gmail.com>
2023-07-13 01:32:14 -0400

    wip: testing next.js

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 
 axios.defaults.baseURL = 'https://spacelaunchnow.me/api/ll/';
 // TODO figure out how to secretly handle this
-// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+axios.defaults.headers.common['Authorization'] = "Token " + process.env.REACT_APP_SPACELAUNCHNOW_API_KEY;
 
 const queryClient = new QueryClient();
 
